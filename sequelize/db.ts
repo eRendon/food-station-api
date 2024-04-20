c055d8be2837217a415ac4c137889ff15704e93e
Edwin Alexander Rendon <erendoncc@gmail.com>
2024-04-20 18:54:55 -0500

    .

--- sequelize/db.ts
+++ sequelize/db.ts
@@ -1,15 +1,11 @@
 import { Sequelize } from 'sequelize'
 import { initProductModel } from '../models/product.model'
 import { initUserModel } from '../models/user.model'
+import pg from 'pg'
 
-const sequelizeApp = new Sequelize({
-  dialect: 'postgres',
-  database: 'station-food',
-  username: 'postgres',
-  password: '050287',
-  host: 'localhost',
-  port: 5432
-});
+const sequelizeApp = new Sequelize('postgresql://eRendon:5Z6jlIQSRmdg@ep-crimson-truth-a5whwe7j.us-east-2.aws.neon.tech/food-station?sslmode=require', {
+  dialectModule: pg
+})
 
 initProductModel(sequelizeApp)
 initUserModel(sequelizeApp)
